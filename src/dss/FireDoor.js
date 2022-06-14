5a8de833cd301afcf82b5a0a88a500c998694726
vandana18519 <103317723+vandana18519@users.noreply.github.com>
2022-06-14 23:06:36 +0100

    Update FireDoor.js

--- src/dss/FireDoor.js
+++ src/dss/FireDoor.js
@@ -2,36 +2,54 @@ import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Checkbox from '@mui/material/Checkbox';
 import ButtonComp from './ButtonComp';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-const FireDoor= () => {
-    const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
-    return (
-        <div style={{display:"block", fontStyle: 'oblique',fontSize: '20px', alignItems: 'center'}}>
-            <div style={{display:'-webkit-inline-flex', fontStyle: 'oblique',fontSize: '20px', alignItems: 'center'}}>
-            Firedoor: <ButtonComp/>
-            </div>
-            <div style={{display:'-webkit-inline-flex', fontStyle: 'oblique',fontSize: '20px', alignItems: 'center'}}>
-            Firedoor: <ButtonComp/>
-            </div>
-            <div style={{display:'-webkit-inline-flex', fontStyle: 'oblique',fontSize: '20px', alignItems: 'center'}}>
-            Firedoor: <ButtonComp/>
-            </div>
-            <div style={{display:'-webkit-inline-flex', fontStyle: 'oblique',fontSize: '20px', alignItems: 'center'}}>
-            Firedoor: <ButtonComp/>
-            </div> 
-            <div style={{display:'-webkit-inline-flex', fontStyle: 'oblique',fontSize: '20px', alignItems: 'center'}}>
-            Firedoor: <ButtonComp/>
-            </div>
-            <div style={{display:'-webkit-inline-flex', fontStyle: 'oblique',fontSize: '20px', alignItems: 'center'}}>
-            Firedoor: <ButtonComp/>
-            </div>
-            <div style={{display:'-webkit-inline-flex', fontStyle: 'oblique',fontSize: '20px', alignItems: 'center'}}>
-            Firedoor: <ButtonComp/>
-            </div>
-            <div style={{display:'-webkit-inline-flex', fontStyle: 'oblique',fontSize: '20px', alignItems: 'center'}}>
-            Firedoor: <ButtonComp/>
-            </div>   
-        </div>
-    )
-}
-export default FireDoor;
+
+export default class Firedoor extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        newItem: "",
+        list: []
+      };
+    }
+  
+  
+    addItem() {
+        console.log("newItem.value");
+      const newItem = {
+        id: 1 + Math.random(),
+        // value: 1 + Math.random()
+      };
+      const list = [...this.state.list];
+      list.push(newItem);
+      this.setState({
+        list,
+        newItem: ""
+      });
+    }
+    render() {
+      return (
+        <div>
+            <button onClick={() => this.addItem()} > 
+              <i class="material-icons"> + </i>
+            </button>
+            <br /> <br />
+            <ul>
+              {this.state.list.map(item => {
+                return (
+                    <div key={item.id} style={{display:'-webkit-inline-flex', fontStyle: 'oblique',fontSize: '20px', alignItems: 'center'}}>
+                        Firedoor: <ButtonComp/>
+                    </div>
+                );
+              })}
+            </ul>
+          </div>
+      );
+    }
+  }
+  
+  ReactDOM.render(<Firedoor />, document.getElementById('root'));
+
+//   disabled={!this.state.newItem.length}
